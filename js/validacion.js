09744b32e32ee6dce9031e86b3abe1f44bc53e4c
Fede-357 <fedelarra.07@gmail.com>
2023-08-09 14:20:45 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,17 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const boton = document.getElementById("regBtn");
+boton.addEventListener('click', function validarFormulario() {
+  var campoNombre = document.getElementById("nombre").value;
+  var campoApellido = document.getElementById("apellido").value;
+  var campoEmail = document.getElementById("email").value;
+  var campoContraseña = document.getElementById("password1").value;
+  var campoRepetirContraseña = document.getElementById("password2").value;
+  var checkboxTerminos = document.getElementById("terminos").checked;
+  if (campoApellido === "" || campoNombre === "" || campoEmail === "" || campoContraseña === "" || campoRepetirContraseña === "" || !checkboxTerminos || campoContraseña.length < 6 || campoContraseña !== campoRepetirContraseña) {
+    showAlertError();
+    return;
+  } showAlertSuccess();
+});
